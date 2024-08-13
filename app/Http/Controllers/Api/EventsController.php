9c4cf896b328dded6b412cea8043f4d55951c957
andre <andrealbuquerque2001@gmail.com>
2024-08-13 11:20:20 -0300

    feat: update controller

--- app/Http/Controllers/Api/EventsController.php
+++ app/Http/Controllers/Api/EventsController.php
@@ -3,145 +3,50 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Middleware\CheckAdminToken;
 use App\Http\Requests\StoreEventsRequest;
-use App\Http\Resources\EventsResource;
-use App\Http\Resources\EventsTitleResource;
-use App\Http\Services\SaveFile;
-use App\Models\Events;
-use App\Models\File;
+use App\Services\EventsService;
 use Illuminate\Http\Request;
 
 class EventsController extends Controller
 {
 
-    protected $saveFile;
+    protected $eventsService;
 
-    public function __construct(SaveFile $saveFile)
+    public function __construct(EventsService $eventsService)
     {
-        $this->saveFile = $saveFile;
+        $this->eventsService = $eventsService;
         $this->middleware('auth:sanctum')->except(['index', 'show', 'showTitle']);
+        $this->middleware(CheckAdminToken::class)->only(['store', 'update', 'destroy']);
     }
-    /**
-     * Display a listing of the resource.
-     */
+
     public function index()
     {
-        try {
-            $events = Events::join('files', 'files.idFile', '=', 'events.idFile')->where('statusEvent', '=', 1)->paginate();
-            return EventsResource::collection($events);
-        } catch (\Exception $e) {
-            return response()->json(['error' => $e->getMessage()], 404);
-        }
+        return $this->eventsService->index();
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(StoreEventsRequest $request)
     {
-        if (!auth()->user()->tokenCan("admin")) {
-            return response()->json(['message' => "unauthorization"], 401);
-        }
-        try {
-            $data = $request->validated();
-            $data['statusEvent'] = 1;
-            // $pathName = $this->saveFile->saveImagem($request->pathName);
-            $idFile = File::insertGetId(
-                [
-                    // 'pathName' => $pathName,
-                    'pathName' => $request->pathName,
-                    'updated_at' => now(),
-                    'created_at' => now(),
-                ]
-            );
-
-            $data['idFile'] = $idFile;
-            Events::create($data);
-            return response()->json(['message' => 'sucess'], 200);
-        } catch (\Exception $e) {
-            return response()->json(['error' => $e->getMessage()], 400);
-        }
+        return $this->eventsService->store($request->validated());
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
-        try {
-            $events = Events::join('files', 'files.idFile', '=', 'events.idFile')->where('idEvents', $id)->first();
-            return new EventsResource($events);
-        } catch (\Exception $e) {
-            return response()->json(['error' => $e->getMessage()], 404);
-        }
+        return $this->eventsService->show($id);
     }
 
     public function showTitle(string $title)
     {
-        try {
-            $events = Events::join('files', 'files.idFile', '=', 'events.idFile')->where('title', 'LIKE', '%' . $title . '%')->get();
-            return new EventsTitleResource($events);
-        } catch (\Exception $e) {
-            return response()->json(['error' => $e->getMessage()], 404);
-        }
+        return $this->eventsService->showTitle($title);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(StoreEventsRequest $request, string $id)
     {
-        if (!auth()->user()->tokenCan("admin")) {
-            return response()->json(['error' => "unauthorization"], 401);
-        }
-        try {
-            $events = Events::findOrFail($id);
-            $data = $request->all();
-            if (isset($data['pathName'])) {
-                $pathName = $this->saveFile->saveImagem($request->pathName);
-                File::where('idFile', $events->idFile)->update(
-                    [
-                        'pathName' => $pathName,
-                        'updated_at' => now(),
-                    ]
-                );
-            }
-
-            $events->update($data);
-            return response()->json(['message' => 'sucess'], 200);
-        } catch (\Exception $e) {
-            return response()->json(['error' => $e->getMessage()], 404);
-        }
+        return $this->eventsService->update($request->validated(), $id);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
-        if (!auth()->user()->tokenCan("admin")) {
-            return response()->json(['error' => "unauthorization"], 401);
-        }
-        try {
-            Events::findOrFail($id)->delete();
-        } catch (\Exception $e) {
-            return response()->json(['errorw' => $e->getMessage()], 404);
-        }
+        return $this->eventsService->destroy($id);
     }
 }
