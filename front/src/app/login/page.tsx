d3264440f163da26072620dceae00dc48736aa04
Andre <andrealbuquerque2001@gmail.com>
2024-08-17 11:00:00 -0300

    feat: create page

---
+++ front/src/app/login/page.tsx
@@ -0,0 +1,67 @@
+'use client'
+import { Login } from '@/action/user/login/route'
+import { ArrowLeft, CircleUserRound, LockKeyhole } from 'lucide-react'
+import Link from 'next/link'
+import { FormEvent, useState } from 'react'
+
+export default function LogIn() {
+  const [error, setError] = useState('')
+  async function handleSearch(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+
+    const formData = new FormData(e.currentTarget)
+    const data = Object.fromEntries(formData)
+    const response = await Login(data)
+    setError(response)
+  }
+
+  return (
+    <div className="flex flex-col justify-center items-center h-screen p-2">
+      <div className="bg-zinc-700 flex flex-col items-center justify-center w-[400px] h-[480px] rounded-xl max-sm:w-[360px] relative">
+        <form onSubmit={handleSearch}>
+          <Link
+            href="/"
+            className="absolute top-0 left-0 mt-4 ml-4 flex flex-row justify-start items-start"
+          >
+            <ArrowLeft /> Voltar
+          </Link>
+          <p className="text-xl mb-7 mt-5">Log In</p>
+          <div className="">
+            <label htmlFor="">E-mail</label>
+            <div className="flex w-[320px] items-center gap-3 rounded-full bg-zinc-800 px-5 py-3 ring-zinc-700">
+              <CircleUserRound className="w-5 h-5 text-zinc-500" />
+              <input
+                type="email"
+                name="email"
+                className="flex-1 bg-transparent text-sm outline-none placeholder:text-zinc-500"
+                required
+              />
+            </div>
+          </div>
+          <div className="mt-5">
+            <label htmlFor="password">Senha</label>
+            <div className="flex w-[320px] items-center gap-3 rounded-full bg-zinc-800 px-5 py-3 ring-zinc-700">
+              <LockKeyhole className="w-5 h-5 text-zinc-500" />
+              <input
+                type="password"
+                name="password"
+                id="password"
+                className="flex-1 bg-transparent text-sm outline-none placeholder:text-zinc-500"
+                required
+              />
+            </div>
+          </div>
+          {error && <span className="text-red-400 text-sm">{error}</span>}
+          <Link href="/user/recover/sendEmail" className="text-sm">
+            Esqueceu senha?
+          </Link>
+          <button className="mt-8 flex h-12 w-[320px] items-center justify-center rounded-full bg-emerald-600 font-semibold text-white">
+            Entrar
+          </button>
+        </form>
+        <div className="h-0.5 w-72 bg-zinc-600 mt-10"></div>
+        <Link href="/user/insert">Não tem conta?</Link>
+      </div>
+    </div>
+  )
+}
