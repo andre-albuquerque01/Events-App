2cfe3e65f372ee04f0173015f7ca02577a88d2a2
andre <andrealbuquerque2001@gmail.com>
2024-08-20 19:45:22 -0300

    feat: create function

---
+++ front/src/data/types/events.ts
@@ -0,0 +1,19 @@
+export interface EventsInterface {
+  users: any
+  event: any
+  idEvents: number
+  id: number
+  title: string
+  description: string
+  price: number
+  department: string
+  occupation: string
+  statusEvent: number
+  idFile: number
+  dateEvent: string
+  timeEvent: string
+  qtdParcelamento: number
+  pathName: string
+  idHasEvents: number
+  name: string
+}
